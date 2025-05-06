ef9c5767e14ca63043e8dc623447a278abe57a3e
rubenalcantarrivera <alcantar.rivera.ruben@gmail.com>
2025-05-05 20:13:43 -0600

    Delete src/api/noaa.js

--- src/api/noaa.js
+++
@@ -1,9 +0,0 @@
-// src/api/noaa.js
-export async function getRecentTides(station) {
-  const url =
-    `https://api.tidesandcurrents.noaa.gov/api/prod/datagetter` +
-    `?product=water_level&application=MARIA&station=${station}` +
-    `&format=json&units=metric&time_zone=lst_ldt&interval=6`;
-  const res = await fetch(url);
-  return await res.json();
-}
