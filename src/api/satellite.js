8269e8441b4393cc86000802fd62c7c342b024c5
rubenalcantarrivera <alcantar.rivera.ruben@gmail.com>
2025-05-05 19:48:07 -0600

    Create satellite.js

---
+++ src/api/satellite.js
@@ -0,0 +1,8 @@
+// src/api/satellite.js
+export async function getNDVI(lat, lon) {
+  // Demo data; reemplaza con tu API real si la tienes
+  return [
+    { date: '2025-04-01', ndvi: 0.65 },
+    { date: '2025-05-01', ndvi: 0.60 }
+  ];
+}
