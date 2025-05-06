4ba438d412a2e96da56723eb0d6da8895b394e98
rubenalcantarrivera <alcantar.rivera.ruben@gmail.com>
2025-05-05 19:49:35 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
+// src/index.js
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import App from './App';
+import './index.css'; // opcional
 
-const root = createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ReactDOM.render(
+  <React.StrictMode><App/></React.StrictMode>,
+  document.getElementById('root')
 );
